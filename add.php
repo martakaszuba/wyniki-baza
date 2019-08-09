904b35e23c7f9966dbe5eb8e2eb2e7f837ca8b47
Marta Kaszuba <48859858+martakaszuba@users.noreply.github.com>
2019-08-09 23:39:35 +0200

    Add files via upload

--- add.php
+++ add.php
@@ -5,19 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Dodaj rekord</title>
 </head>
-<style>
-#main {
-	text-align:center;
-	margin:50px auto;
-}
-
-#err {
-	color:red;
-	margin:10px;
-}
-</style>
 <body>
 <div id="main">
 	<form method="POST">
@@ -35,7 +25,7 @@ if (isset($_POST["submit"])){
 	$surname = trim($surname);
 	$age = trim($age);
 	if (strlen($age) ===0 || strlen($name) ===0 || strlen($surname) ===0 || !is_numeric($age)){
-echo '<p id="err">Wpisz poprawne dane!<p>';
+echo '<h5 id="err">Wpisz poprawne dane!</h5>';
 	}
 	else {
 		$conn = new mysqli("localhost", "root", "", "imiona");
@@ -46,9 +36,9 @@ echo '<p id="err">Wpisz poprawne dane!<p>';
 		$stmt = $conn->prepare($sql);
 		$stmt->bind_param("ssi", $name, $surname, $age);
 		$stmt->execute();
-		print_r($conn);
 		$stmt->close();
-        $conn->close();
+		$conn->close();
+		header("Location:index.php");
 	}
 }
 
--- show.php
+++ show.php
@@ -1,30 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Dodaj rekord</title>
 </head>
 <body>
-    <table>
-        <?php
-    $conn = mysqli_connect("localhost", "root", "", "baz");
- mysqli_set_charset($conn,"utf8");   
- $sql = "SELECT * FROM filmy"; 
- $result = mysqli_query($conn, $sql);
- while ($row = mysqli_fetch_assoc($result)){
-     echo "<tr>";
-            echo "<td>".$row["id"]."</td>";
-            echo "<td>".$row["tytul"]."</td>";
-        }
-        echo "</tr>";
-        mysqli_close($conn);
+<table>
+<?php
+$conn = new mysqli("localhost", "root", "", "imiona");
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		$stmt = $conn->prepare("SELECT * FROM imiona");
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows === 0) exit('No rows');
+echo "<tr class='b'>";
+  echo "<td>id</td>";
+  echo "<td>Imię</td>";
+  echo "<td>Nazwisko</td>";
+  echo "<td>Wiek</td>";
+  echo "</tr>";
+while($row = $result->fetch_assoc()) {
+  $id = $row['id'];
+  $name = $row['name'];
+  $surname = $row['surname'];
+  $age = $row['age'];
+  echo "<tr>";
+  echo "<td>".$id. ".</td>";
+  echo "<td>".$name. "</td>";
+  echo "<td>".$surname. "</td>";
+  echo "<td>".$age. "</td>";
+  echo "</tr>";
+}
+$stmt->close();
 ?>
 </table>
 </body>
 </html>
-
-
 
 

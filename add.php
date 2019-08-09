1bf4623a1ddd6564230a658c9371ce6ccdf183e4
Marta Kaszuba <48859858+martakaszuba@users.noreply.github.com>
2019-08-09 23:17:15 +0200

    Add files via upload

--- add.php
+++ add.php
@@ -1,34 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Dodaj rekord</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<title>Dodaj rekord</title>
 </head>
+<style>
+#main {
+	text-align:center;
+	margin:50px auto;
+}
+
+#err {
+	color:red;
+	margin:10px;
+}
+</style>
 <body>
-    <div id="main">
-    <form method ="POST">
-<p>Dodaj nowy tytuł: <input type="text" name="tt"></p>
-<button name="submit" class="btn btn-secondary">Dodaj</button>
-</form>
-</div>
-<?php
+<div id="main">
+	<form method="POST">
+	<p>Wpisz imię: <input type="text" name="name" id="name"></p>
+	<p>Wpisz nazwisko: <input type="text" name="surname" id="surname"></p>
+	<p>Wpisz wiek: <input type="number" name="age" id="age"></p>
+	<button name="submit" class="btn btn-secondary">Dodaj</button>
+	</form>
+	<?php
 if (isset($_POST["submit"])){
-$conn = mysqli_connect("localhost", "root", "", "baz");
-$title = mysqli_real_escape_string($conn, $_POST['tt']);
-$sql = "INSERT INTO filmy (tytul) VALUES('$title')";
-$result = mysqli_query($conn, $sql);
-mysqli_close($conn);
-header("Location:ind.php");
+	$name = $_POST["name"];
+	$surname = $_POST["surname"];
+	$age = $_POST["age"];
+	$name = trim($name);
+	$surname = trim($surname);
+	$age = trim($age);
+	if (strlen($age) ===0 || strlen($name) ===0 || strlen($surname) ===0 || !is_numeric($age)){
+echo '<p id="err">Wpisz poprawne dane!<p>';
+	}
+	else {
+		$conn = new mysqli("localhost", "root", "", "imiona");
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		$sql ="INSERT INTO imiona (name, surname, age) VALUES (?,?,?)";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ssi", $name, $surname, $age);
+		$stmt->execute();
+		print_r($conn);
+		$stmt->close();
+        $conn->close();
+	}
 }
 
 ?>
+	</div>
 </body>
 </html>
-
-
 
 
--- index.php
+++ index.php
@@ -1,35 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Marta Kaszuba - wyniki z bazy danych</title>
-  
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Baza danych - Marta Kaszuba</title>
 </head>
-
 <body>
-    <div id="main">
-    <form action="show.php" method="post">
-    <button name="show" class="btn btn-secondary">Pokaż</button>
+<div id="main">
+<form method="POST">
+<button name="show" class="btn btn-secondary">Pokaż</button>
+<button name="add" class="btn btn-secondary">Dodaj</button>
+<button name="change" class="btn btn-secondary">Zmień</button>
+<button name="delete" class="btn btn-secondary">Usuń</button>
+<button name="find" class="btn btn-secondary">Wyszukaj</button>
 </form>
-<form action="change.php" method="post">
-    <button name="change" class="btn btn-secondary">Zmień</button>
-</form>
-<form action="add.php" method="post">
-    <button name="add" class="btn btn-secondary">Dodaj</button>
-</form>
-<form action="delete.php" method="post">
-    <button name="delete" class="btn btn-secondary">Usuń</button>
-</form>
-<form action="find.php" method="post">
-    <button name="find" class="btn btn-secondary">Wyszukaj</button>
-</form>
-    </div>
+</div>
+<?php
+if (isset($_POST["show"])){
+header("Location:show.php");
+}
+else if (isset($_POST["add"])){
+	header("Location:add.php");	
+}
+else if (isset($_POST["change"])){
+	header("Location:change.php");	
+}
+else if (isset($_POST["delete"])){
+	header("Location:delete.php");	
+}
+else if (isset($_POST["find"])){
+	header("Location:find.php");	
+}
+
+?>
+
+
 </body>
 </html>
-
 
 
